6a2bf9e9e07abe59817e56a81baeb31c4aeb4a9f
Berdan Akyurek <berdanakyurek17@gmail.com>
2024-07-08 09:31:04 +0300

    update script

---
+++ update_xkb.sh
@@ -0,0 +1,9 @@
+# Run this as root
+
+cd /usr/share/X11/xkb
+rm -rf *
+rm -rf .git
+git init
+git remote add origin git@github.com:berdanakyurek/x11-keyboard-configuration.git
+git pull origin main
+setxkbmap -layout us
